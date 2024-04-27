34bc5e940edd8d59654c2d4a9bd67ccd84e4b54f
Diego Gerwig <diegogerwig@gmail.com>
2024-04-27 10:46:23 +0200

    WIP log reg train

--- src/logreg/logreg_train copy.py
+++ src/logreg/logreg_train copy.py
@@ -26,7 +26,66 @@ columns_to_drop = []
 houses = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
 
 
+# class MyLogisticRegression:
+#     def __init__(self, thetas, lr=LEARNING_RATE, max_iter=MAX_ITERATIONS):
+#         self.thetas = thetas  # Ahora es una lista de thetas para cada casa
+#         self.lr = lr
+#         self.max_iter = max_iter
+#         self.loss_history = {house: [] for house in houses}
 
+#     def sigmoid(self, z):
+#         return 1 / (1 + np.exp(-z))
+    
+#     def loss(self, h, y):
+#         return -np.mean(y * np.log(h) + (1 - y) * np.log(1 - h))
+    
+#     def fit(self, X, y, model_name, progress_bar=None):
+#         m = len(y)
+#         X = np.hstack((np.ones((m, 1)), X))
+#         for i, (theta, house) in enumerate(zip(self.thetas, houses)):
+#             for _ in tqdm(range(self.max_iter), desc=f'Training {model_name} for {house}'):
+#                 print("Shape of X:", X.shape)
+#                 print("Shape of theta:", theta.shape)
+#                 h = self.sigmoid(X.dot(theta.reshape(-1, 1)))
+#                 error = h - y
+#                 gradient = X.T.dot(error) / m
+#                 updated_theta = theta - self.lr * gradient
+#                 self.thetas[i] = updated_theta
+#                 print("🟣 Shape of updated theta:", updated_theta.shape)
+#                 current_loss = self.loss(h, y)
+#                 self.loss_history[house].append(current_loss)
+#                 if progress_bar:
+#                     progress_bar.update(1)
+
+#     def plot_loss_history(self, model_name):
+#         plt.figure(figsize=(15, 10))
+#         line_styles = ['-', '--', '-.', ':']
+#         for i, house in enumerate(houses):
+#             house_loss_history = np.array(self.loss_history[house])
+#             line_style = line_styles[i % len(line_styles)]
+#             plt.plot(house_loss_history, label=house, linestyle=line_style)
+#         plt.title(f'Loss Function History {model_name}')
+#         plt.xlabel('Iteration')
+#         plt.ylabel('Loss')
+#         plt.grid(True)
+#         plt.legend()
+#         plt.tight_layout()
+#         plt.show(block=False)
+#         os.makedirs(PLOTS_DIR, exist_ok=True)
+#         save_path = os.path.join(PLOTS_DIR, f'plot_LOSS_history_{model_name}.png')
+#         plt.savefig(save_path)
+#         print('\n⚪️ Plot saved as: {}\n'.format(save_path))
+#         input('\nPress Enter to continue...\n')
+#         plt.close()
+    
+#     def predict(self, X):
+#         m = X.shape[0]
+#         X = np.hstack((np.ones((m, 1)), X))
+#         probabilities = []
+#         for theta in self.thetas:  # Iterar sobre las thetas
+#             probabilities.append(self.sigmoid(X.dot(theta)))
+#         probabilities = np.array(probabilities)
+#         return np.argmax(probabilities, axis=0)
 
 
 def normalize_xset(x):
@@ -34,24 +93,6 @@ def normalize_xset(x):
     stds = np.std(x, axis=0)
     X_norm = (x - means) / stds
     return X_norm, means, stds
-
-
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-
-def gradient_descent(X, y, theta, alpha, num_iters):
-    m = len(y)
-    J_history = []
-
-    for _ in tqdm(range(num_iters)):
-        h = sigmoid(np.dot(X, theta))
-        J = (-1 / m) * np.sum(y * np.log(h) + (1 - y) * np.log(1 - h))
-        J_history.append(J)
-        gradient = (1 / m) * np.dot(X.T, (h - y))
-        theta -= alpha * gradient
-
-    return theta, J_history
-
 
 
 def train(filename):
@@ -161,37 +202,46 @@ def train(filename):
         print(f"Labels for {house}: \t{y_trains[i][:20]}")
     input('\nPress Enter to continue...\n')
 
-    thetas = []  
-    for _ in range(4):  
-        theta = np.zeros((nb_features + 1, 1))  
+    thetas = []  # Aquí se inicializan los thetas para cada casa
+    for _ in range(4):  # Para cada casa
+        theta = np.zeros((nb_features + 1, 1))  # Inicializar theta como un vector de ceros
+        # theta = np.expand_dims(theta, axis=1)  # Expandir la dimensión de theta
         thetas.append(theta)
     print('\nINITIALIZE THETAS')
     for i, theta in enumerate(thetas):
         print(f"Parameters for house {houses[i]} (shape {theta.shape}): \n{theta}")
     input('\nPress Enter to continue...\n')
 
-    for i, house in enumerate(houses):
-        print(f"Training for house: {house}")
-        X = np.hstack((np.ones((X_norm.shape[0], 1)), X_norm))
-        theta, J_history = gradient_descent(X, y_trains[i].reshape(-1, 1), thetas[i], LEARNING_RATE, MAX_ITERATIONS)
-        thetas[i] = theta
-
-    try:
-        with open(PARAMS_FILE_PATH, 'w', newline='') as file:
-            writer = csv.writer(file)
-            for theta in thetas:
-                theta_str = ','.join([str(val) for val in theta.flatten()])
-                writer.writerow([theta_str])
-        print('\n⚪️ Parameters file saved as: {}\n'.format(PARAMS_FILE_PATH))
-    except FileNotFoundError:
-        print('❌ Error: File not found {}'.format(PARAMS_FILE_PATH),
-              file=sys.stderr)
-        exit(1)
-    except Exception as e:
-        print('❌ Error:', e, file=sys.stderr)
-        exit(1)
+    # models = []
+    # for i, (theta, y_train, house) in enumerate(zip(thetas, y_trains, houses)):
+    #     model = MyLogisticRegression(theta, lr=LEARNING_RATE, max_iter=MAX_ITERATIONS)
+    #     progress_bar = tqdm(total=MAX_ITERATIONS, desc=f'Training {house}', disable=True)
+    #     print("🟢 Shape of X_norm:", X_norm.shape)
+    #     print("🟢 Shape of y_train:", y_train.shape)
+    #     model.fit(X_norm, y_train, house, progress_bar=progress_bar)
+    #     progress_bar.close()
+    #     models.append(model)
 
 
+    # model.plot_loss_history('All Houses')
+
+    # Save the parameters to a CSV file
+    # try:
+    #     with open(PARAMS_FILE_PATH, 'w') as file:
+    #         writer = csv.writer(file)
+    #         writer.writerow(["theta"])
+    #         for model in models:
+    #             last_theta = model.thetas[:, -1]  # Corregir aquí
+    #             theta_str = ','.join([f'{val}' for val in last_theta])
+    #             writer.writerow([theta_str])
+    #     print('\n⚪️ Parameters file saved as: {}\n'.format(PARAMS_FILE_PATH))
+    # except FileNotFoundError:
+    #     print('❌ Error: File not found {}'.format(PARAMS_FILE_PATH),
+    #           file=sys.stderr)
+    #     exit(1)
+    # except Exception as e:
+    #     print('❌ Error:', e, file=sys.stderr)
+    #     exit(1)
 
 
 def main():
