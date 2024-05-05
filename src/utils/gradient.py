e6561b58d0172d12357abb71a5d5a934abd46e1f
Diego Gerwig <diegogerwig@gmail.com>
2024-05-05 18:01:44 +0200

    cleaning & improving files

--- src/utils/gradient.py
+++ src/utils/gradient.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 
 def sigmoid(z):
-    sig = 1 / (1 + np.exp(-z))
+    sig = 1 / (1 + np.exp(-z)) # Sigmoid function (logistic function)
     return sig
 
 
@@ -11,11 +11,11 @@ def gradient_descent(X, y, theta, lr, num_iters, threshold=1e-5):
     m = len(y)
     J_history = []
     for _ in tqdm(range(num_iters)):
-        h = sigmoid(np.dot(X, theta))
-        J = (-1 / m) * np.sum(y * np.log(h) + (1 - y) * np.log(1 - h))
+        h = sigmoid(np.dot(X, theta)) # Hypothesis function (sigmoid)
+        J = (-1 / m) * np.sum(y * np.log(h) + (1 - y) * np.log(1 - h)) # Cost function
         J_history.append(J)
-        gradient = (1 / m) * np.dot(X.T, (h - y))
-        theta -= lr * gradient
-        if len(J_history) > 1 and abs(J_history[-1] - J_history[-2]) < threshold:
+        gradient = (1 / m) * np.dot(X.T, (h - y)) # Gradient of the cost
+        theta -= lr * gradient  # Update theta with gradient descent
+        if len(J_history) > 1 and abs(J_history[-1] - J_history[-2]) < threshold: # Convergence check with threshold
             break
     return theta, J_history
